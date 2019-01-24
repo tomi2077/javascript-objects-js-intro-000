7ca27f386ad043dc4d687673d57d39a19708c71c
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-24 00:59:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { Chris_Brown : "Freaky Friday", Eminem: "Sing fo a Moment"};
 var updatePlaylist = (playlist, artistName, songTitle) => {
   playlist[artistName] = songTitle;
 }
+
+var removeFromPlaylist = (playlist, artistName) => {
+  delete removeFromPlaylist.playlist;
+}
